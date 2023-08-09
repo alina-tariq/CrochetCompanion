c706c2af30e7bf79a65ea16492c7b636afcf8d7f
alina-tariq <alina.tariq@ryerson.ca>
2023-08-09 17:56:20 -0400

    Initial Commit

---
+++ Crochet Helper/Crochet_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crochet_HelperApp.swift
+//  Crochet Helper
+//
+//  Created by Alina Tariq on 2023-08-09.
+//
+
+import SwiftUI
+
+@main
+struct Crochet_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
