3eef1d1d3a6c3e87c7bec792063b134f56462733
alina-tariq <alina.tariq@ryerson.ca>
2023-08-13 15:54:17 -0400

    added app icon and launch screen + renamed app

--- CrochetCompanion/CrochetCompanionApp.swift
+++ CrochetCompanion/CrochetCompanionApp.swift
@@ -1,6 +1,6 @@
 //
-//  CrochetHelperApp.swift
-//  CrochetHelper
+//  CrochetCompanionApp.swift
+//  CrochetCompanion
 //
 //  Created by Alina Tariq on 2023-08-09.
 //
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct CrochetHelperApp: App {
+struct CrochetCompanionApp: App {
     var body: some Scene {
         WindowGroup {
             let _ = UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
