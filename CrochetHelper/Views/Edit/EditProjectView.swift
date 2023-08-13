3ab1a99566982358536fb278906f48f16bd530f4
alina-tariq <alina.tariq@ryerson.ca>
2023-08-13 01:55:31 -0400

    added views to edit objects (the cancel and save buttons don't work yet)

---
+++ CrochetHelper/Views/Edit/EditProjectView.swift
@@ -0,0 +1,20 @@
+//
+//  EditFileView.swift
+//  CrochetHelper
+//
+//  Created by Alina Tariq on 2023-08-13.
+//
+
+import SwiftUI
+
+struct EditFileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditFileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditFileView()
+    }
+}
---
+++ CrochetHelper/Views/Edit/EditYarnView.swift
@@ -0,0 +1,20 @@
+//
+//  EditYarnView.swift
+//  CrochetHelper
+//
+//  Created by Alina Tariq on 2023-08-13.
+//
+
+import SwiftUI
+
+struct EditYarnView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditYarnView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditYarnView()
+    }
+}
